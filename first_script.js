bf23f6be4efef408abf76d12f94afcef01dd558c
LZLZ15 <97078066+LZLZ15@users.noreply.github.com>
2022-01-04 12:31:24 +0800

    add html

---
+++ first_script.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    $("#submit").click(function () {
+        $('body').append('<img src="../efficiency_RF_CV.png"></img>')
+    });
+});
+
+// $(document).ready(function(){
+//     $("#submit").click(function(e) {
+//         e.preventDefault(); // prevent from submitting
+//         $("#formImg").show();
+//     });
+// });
